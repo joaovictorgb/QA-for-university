a7d6b9aec5b1ee5358b359533b8090fb2b04d8ab
fflins <felippelins09@gmail.com>
2024-05-11 22:14:23 -0300

    update testes

--- LAB7/src/testes/VisitorCalculaAreaTest.java
+++ LAB7/src/testes/VisitorCalculaAreaTest.java
@@ -12,17 +12,23 @@ public class VisitorCalculaAreaTest {
 	
 	VisitorIF ca;
  	Retangulo r;
+	Circulo c;
 
 	@Before
 	public void setup() {
 		ca = new VisitorCalculaArea();
 		r = new Retangulo(2,4);
-		
+		c = new Circulo(2);
 	}
 	
 	
 	@Test
 	public void testAreaRetangulo() {
 		assertEquals(8, ca.visitaRetangulo(r), 0.1);
+	}
+
+	@Test
+	public void testAreaCirculo(){
+		assertEquals(12.58 , ca.visitaCirculo(c), 0.1);
 	}
 }
